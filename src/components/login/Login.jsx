2d988f52822343eda8d1cb21a5d0cb639f488255
khairul-01 <khairul431743@gmail.com>
2023-10-04 07:38:40 +0600

    show icon and password verification added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,104 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase/firebase.config";
+import { config } from "localforage";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+   const [registerError, setRegisterError] = useState('');
+   const [registerSuccess, setRegisterSuccess] = useState('');
+   const [toggle, setToggle] = useState(false);
+
+   const emailRef = useRef(null);
+
+   const handleLogin = e => {
+      e.preventDefault();
+      const email = e.target.email.value;
+      const password = e.target.password.value;
+      console.log(email, password);
+
+      // reset error and success
+      setRegisterError('');
+      setRegisterSuccess('');
+      if (password.length < 6) {
+         setRegisterError('Password should be at least 6 characters');
+         return;
+      }
+      else if (!/[A-Z]/.test(password)) {
+         setRegisterError('Password should have at least one uppercase letter characters');
+         return;
+      }
+
+      signInWithEmailAndPassword(auth, email, password)
+         .then(result => {
+            console.log(result.user);
+            setRegisterSuccess('User created Successfully');
+         })
+         .catch(error => {
+            console.error(error);
+            setRegisterError(error.message);
+         })
+   }
+   const handleResetPassword = () => {
+      const email = emailRef.current.value;
+      if (!email) {
+         console.log('please provide email address');
+         return;
+      }
+      else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+         console.log('Please provide valid email address');
+         return;
+      }
+
+      // send validation email
+      sendPasswordResetEmail(auth, email)
+         .then(() => {
+            alert('Please check your email.');
+         })
+         .catch(error => {
+            console.log(error);
+         })
+   }
    return (
       <div>
-         <h1 className="text-3xl">Please Log In</h1>
+         <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+               <div className="text-center lg:text-left">
+                  <h1 className="text-5xl font-bold">Login now!</h1>
+                  <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+               </div>
+               <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                  <form onSubmit={handleLogin} className="card-body">
+                     <div className="form-control">
+                        <label className="label">
+                           <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" name="email" ref={emailRef} className="input input-bordered" required />
+                     </div>
+                     <div className="form-control">
+                        <label className="label">
+                           <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                           <a onClick={handleResetPassword} href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                     </div>
+                     <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                     </div>
+                  </form>
+                  {
+                     registerError && <p className="text-red-800 font-extrabold">{registerError}</p>
+                  }
+                  {
+                     registerSuccess && <p className="text-green-800 font-extrabold">{registerSuccess}</p>
+                  }
+                  <p>New to this website? Please <Link to='/register' className="underline btn">register</Link></p>
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,21 +1,42 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 import { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
    const [registerError, setRegisterError] = useState('');
+   const [registerSuccess, setRegisterSuccess] = useState('');
+   const [toggle, setToggle] = useState(false);
 
    const handleSubmit = e => {
       e.preventDefault();
       const email = e.target.email.value;
       const password = e.target.password.value;
-      console.log(email, password);
+      const check = e.target.terms.checked;
+      console.log(email, password, check);
 
       setRegisterError('');
+      setRegisterSuccess('');
+      if (password.length < 6) {
+         setRegisterError('Password should be at least 6 characters');
+         return;
+      }
+      else if (!/[A-Z]/.test(password)) {
+         setRegisterError('Password should have at least one uppercase letter characters');
+         return;
+      }
+      else if (!check) {
+         setRegisterError('Please check our terms & conditions');
+         return;
+      }
 
       createUserWithEmailAndPassword(auth, email, password)
-         .then(result => result.user)
+         .then(result => {
+            console.log(result.user);
+            setRegisterSuccess('User created Successfully');
+         })
          .catch(error => {
             console.error(error);
             setRegisterError(error.message);
@@ -26,13 +47,33 @@ const Register = () => {
          <div className="w-1/2 mx-auto">
             <form onSubmit={handleSubmit}>
                <h1 className="text-3xl mb-4">Please Register</h1>
-               <input className="w-3/5 border-2 rounded-md px-4 py-2 mb-4" type="email" name="email" id="" placeholder="Email Address" /> <br />
-               <input className="w-3/5 border-2 rounded-md px-4 py-2 mb-4" type="password" name="password" id="" placeholder="Password" /> <br />
+               <input className="w-3/5 border-2 rounded-md px-4 py-2 mb-4" type="email" name="email" id="" placeholder="Email Address" required /> <br />
+               <div className="flex items-center mb-4 relative w-3/5 ">
+                  <input
+                     className="border-2 rounded-md px-4 py-2 w-full"
+                     type={toggle ? 'text' : 'password'}
+                     name="password"
+                     id="" placeholder="Password" required />
+                  <span onClick={() => setToggle(!toggle)} className="bg-none text-2xl absolute right-2">
+                     {
+                        toggle ? <AiFillEye /> : <AiFillEyeInvisible />
+                     }
+                  </span>
+               </div>
+
+               <div className="mb-3">
+                  <input type="checkbox" name="terms" id="terms" />
+                  <label className="ml-2" htmlFor="terms">Accept out terms & conditions</label>
+               </div>
                <input className="w-3/5 mb-4 btn btn-secondary" type="submit" value="Register" />
             </form>
             {
                registerError && <p className="text-red-800 font-extrabold">{registerError}</p>
             }
+            {
+               registerSuccess && <p className="text-green-800 font-extrabold">{registerSuccess}</p>
+            }
+            <p>Already have an account? Please <Link to='/login' className="underline btn">login</Link></p>
          </div>
       </div>
    );
